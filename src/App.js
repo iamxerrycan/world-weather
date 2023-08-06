b91b947c206987aca1d093716f06f9a18da4c388
rajshish333 <rajshish333@gmail.com>
2023-08-06 15:18:46 +0530

    weather creation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+class App extends Component {
+  render() {
+    return (
+    <Hello/>
+    );
+  }
+}
+
+export default App;
